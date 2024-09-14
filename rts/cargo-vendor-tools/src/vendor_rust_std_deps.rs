14d68cc9414cf1a360b2054781abcfc7ded2e69d
Gabor Greif <gabor@dfinity.org>
2024-09-14 20:24:47 +0200

    harmless change

--- rts/cargo-vendor-tools/src/vendor_rust_std_deps.rs
+++ rts/cargo-vendor-tools/src/vendor_rust_std_deps.rs
@@ -7,7 +7,7 @@ use serde_json as json;
 use sha2::{Digest, Sha256};
 
 static DESCR: &str = "
-Given a Rust toolchain path and a vendor directory, this program vendors Rust std dependencies in
+XGiven a Rust toolchain path and a vendor directory, this program vendors Rust std dependencies in
 the given vendor directory. This directory can then be used to build packages with `-Zbuild-std`
 parameters in nix.
 ";
