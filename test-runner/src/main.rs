9ca3f1b9945f57acefdd6bf15550f2089b281bbe
Alexandru Uta <alexandru.uta@alexandru-uta-d6g2v5j0ny.mulley-anaconda.ts.net>
2026-02-04 10:58:26 +0100

    Add more functionality.

--- test-runner/src/main.rs
+++ test-runner/src/main.rs
@@ -1,8 +1,10 @@
 mod test_runner;
 use crate::test_runner::SubnetType;
-use dialoguer::FuzzySelect;
+use inquire::MultiSelect;
+use std::env;
 use std::io::Read;
 use std::process::Command;
+use std::time::{Duration, Instant};
 use walkdir::WalkDir;
 
 /// The program reads stdin where the .drun file contents are piped in.
@@ -29,6 +31,91 @@ fn run_legacy_mode(args: Vec<String>) {
     test_runner::run_cmdline_test(buffer, subnet_type);
 }
 
+/// The program offers the user a list of tests to choose from.
+/// A summary of the results of the tests is then printed out.
+fn run_interactive_mode() {
+    let Ok(path) = env::current_dir() else {
+        println!("Could not determine current directory. Aborting.");
+        return;
+    };
+    if !path.ends_with("motoko") {
+        println!("Current path: {:?}", path.display());
+        println!(
+            "test-runner --interactive should be run in the top-level motoko/ main repo directory only."
+        );
+        return;
+    }
+
+    let test_dirs = ["test/run-drun", "test/run", "test/fail"];
+
+    let mut tests: Vec<String> = Vec::new();
+    for test_dir in test_dirs {
+        let local_tests: Vec<String> = WalkDir::new(test_dir)
+            .max_depth(1)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|f| f.file_type().is_file())
+            .filter_map(|e| e.path().to_str().map(|s| s.to_string()))
+            .filter(|f| f.ends_with(".mo") || f.ends_with(".drun"))
+            .collect();
+
+        tests.extend(local_tests);
+    }
+
+    let Ok(selection) = MultiSelect::new(
+        "Chose a motoko test to run.\nYou can filter by name or navigate.\nFilter:",
+        tests,
+    )
+    .prompt() else {
+        println!("Error selecting tests.");
+        std::process::exit(1);
+    };
+    let start_time = Instant::now();
+    let test_results = selection.into_iter().map(run_single_test).collect();
+    let duration = start_time.elapsed();
+    print_summary(test_results, duration);
+}
+
+fn print_summary(test_results: Vec<SingleTestResult>, duration: Duration) {
+    println!("You ran {:?} tests in {:?}", test_results.len(), duration);
+    let failed: Vec<&SingleTestResult> = test_results.iter().filter(|t| !t.success).collect();
+    let successful_no = test_results.len() - failed.len();
+    println!("\t --> {successful_no} tests ran successfully.");
+
+    for test_result in failed {
+        println!("Test {:?} failed.", test_result.test_name);
+        println!("Stderr: {:?}", test_result.stderr);
+        println!("Stdout: {:?}", test_result.stdout);
+    }
+}
+
+struct SingleTestResult {
+    success: bool,
+    stdout: String,
+    stderr: String,
+    test_name: String,
+}
+
+fn run_single_test(test_name: String) -> SingleTestResult {
+    let running_test = Command::new("test/run.sh")
+        .arg("-d")
+        .arg(test_name.clone())
+        .output()
+        .unwrap_or_else(|_| {
+            panic!(
+                "OS-related error. Failed to run test: {:?}.",
+                test_name.as_str()
+            )
+        });
+
+    SingleTestResult {
+        success: running_test.clone().status.success(),
+        stdout: String::from_utf8_lossy(&running_test.stdout).to_string(),
+        stderr: String::from_utf8_lossy(&running_test.stderr).to_string(),
+        test_name: test_name.clone(),
+    }
+}
+
 fn main() {
     // Parse command line arguments.
     let args = std::env::args().collect::<Vec<String>>();
@@ -48,38 +135,7 @@ fn main() {
     } else if args.contains(&"--run".to_string()) {
         run_legacy_mode(args);
     } else if args.contains(&"--interactive".to_string()) {
-        let tests_path = "test/";
-        let test_dirs = ["run-drun", "run", "fail"];
-
-        let mut tests: Vec<String> = Vec::new();
-        for test_dir in test_dirs {
-            let crnt_path = tests_path.to_string() + test_dir;
-            let local_tests: Vec<String> = WalkDir::new(crnt_path)
-                .into_iter()
-                .filter_map(|e| e.ok())
-                .filter(|f| f.file_type().is_file())
-                .filter_map(|e| e.path().to_str().map(|s| s.to_string()))
-                .filter(|f| f.ends_with(".mo") || f.ends_with(".drun"))
-                .collect();
-
-            tests.extend(local_tests);
-        }
-
-        let selection = FuzzySelect::new()
-            .with_prompt("Chose a motoko test to run. You can filter by name or navigate.")
-            .items(&tests)
-            .interact()
-            .unwrap();
-
-        println!("Running test: {}", tests[selection]);
-
-        let mut running_test = Command::new("test/run.sh")
-            .arg("-d")
-            .arg(tests[selection].clone())
-            .spawn()
-            .expect("failed to run test.");
-
-        let _ = running_test.wait().expect("failed to run test");
+        run_interactive_mode();
     }
 }
 
