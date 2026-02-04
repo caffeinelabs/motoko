56dd0d5af8821d3afa29b5a981a4d98819568aee
Claudio Russo <claudio@dfinity.org>
2026-02-04 15:08:25 +0000

    don't do actor classes for now

--- src/mo_idl/mo_to_idl.ml
+++ src/mo_idl/mo_to_idl.ml
@@ -252,9 +252,11 @@ module MakeState() = struct
         | Func (Local, Returns, [tb], ts1, [t2]) ->
           let args = List.map arg_typ (List.map (open_ [Non]) ts1) in
           let (_, _, actor_typ) = as_async (normalize (open_ [Non] t2)) in
-          let viewer_tfs = gather_views [] dfs in
+(*          let viewer_tfs = gather_views [] dfs in
           let extended_actor_typ = extend_obj actor_typ viewer_tfs in
           let actor = typ extended_actor_typ in
+ *)
+          let actor = typ actor_typ in
           Some (I.ClassT (args, actor) @@ cub.at)
         | _ -> assert false
        )
