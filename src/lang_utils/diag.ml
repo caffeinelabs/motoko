e074ce5ec13236bb0f0f5d0a67315a213324b8f3
Kamil Listopad <listopadkamil@gmail.com>
2026-02-11 15:43:32 +0100

    corner cases for M0237

--- src/lang_utils/diag.ml
+++ src/lang_utils/diag.ml
@@ -9,7 +9,6 @@ type message = {
   at : Source.region;
   cat : string;
   text : string;
-  (* TODO: only compute edits when json output is enabled *)
   edits : text_edit list;
 }
 type messages = message list
