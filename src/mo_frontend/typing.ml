898e6c11f7a721f4eb6947fb3ef720b7a6f11a3d
Gabor Greif <gabor@dfinity.org>
2026-02-26 12:16:36 +0100

    diagnostics

--- src/mo_frontend/typing.ml
+++ src/mo_frontend/typing.ml
@@ -935,14 +935,15 @@ and check_typ' env typ : T.typ =
   | WeakT typ ->
     T.Weak (check_typ env typ)
   | FromCandidT candid ->
-    let result = Idllib.Pipeline.parse_string ~left:(Some typ.at.left) candid in
-    match result with
-    | Ok ((prog, wut), msgs) ->
+    match Idllib.Pipeline.parse_string ~left:(Some typ.at.left) candid with
+    | Error msgs -> Diag.add_msgs env.msgs msgs; raise Recover
+    | Ok ((prog, _wut), msgs) ->
       (*Printf.eprintf "WUT: %s\n" wut;*)
-      (match Idllib.Typing.check_prog Idllib.Typing.Env.empty prog with
+      Diag.add_msgs env.msgs msgs;
+      match Idllib.Typing.check_prog Idllib.Typing.Env.empty prog with
+       | Error msgs -> Diag.add_msgs env.msgs msgs; raise Recover
        | Ok ((s, Some t), _) -> Mo_idl.Idl_to_mo.check_typ s t
-       | _ -> failwith "ERROR1")
-    | _ -> failwith "ERROR2"
+       | Ok ((s, None), _) -> error env typ.at "M0X42" "imported `actor` type doesn't specify a single `service`"
 
 and check_typ_def env at (id, typ_binds, typ) : T.kind =
   let cs, tbs, te, ce = check_typ_binds {env with pre = true} typ_binds in
--- test/run/candid-service.mo
+++ test/run/candid-service.mo
@@ -1,4 +1,4 @@
-func _foo() : async/*WHY NEEDED?*/ from_candid "type T = int; service B : { \"getInt\": () -> (T); }" {
+func _foo() : async/*WHY NEEDED?*/ from_candid "type T = int; service B : { \"getInt\": () -> (Ta); } X" {
     actor { public func getInt() : async Int { 42 } }
 }
 
