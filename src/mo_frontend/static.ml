6fcacaccb9aa322dc6b1ec58ee038f8cb6ab621c
Alexandru Uta <alexandru.uta@alexandru-uta-d6g2v5j0ny.mulley-anaconda.ts.net>
2026-03-27 15:19:41 +0100

    make checks less strict.

--- src/mo_frontend/static.ml
+++ src/mo_frontend/static.ml
@@ -33,35 +33,44 @@ let pat_err m at =
        "type"
        "only trivial patterns allowed in static expressions")
 
-let rec exp m e = match e.it with
+let rec exp ?(allow_system = false) m e  = match e.it with
   (* Plain values *)
-  | HoleE (s, e) -> exp m !e
+  | HoleE (s, e) -> exp ~allow_system m !e 
   | (PrimE _ | LitE _ | ActorUrlE _ | FuncE _) -> ()
-  | (TagE (_, exp1) | OptE exp1) -> exp m exp1
-  | TupE es -> List.iter (exp m) es
+  | (TagE (_, exp1) | OptE exp1) -> exp ~allow_system m exp1 
+  | TupE es -> List.iter (exp ~allow_system m) es
   | ArrayE (mut, es) ->
     begin
       match mut.it with
-      | Const ->  List.iter (exp m) es
+      | Const ->  List.iter (exp ~allow_system m) es
       | Var -> err m e.at
     end
   | ObjBlockE (eo, _, _, dfs) ->
-    Option.iter (exp m) eo; dec_fields m dfs
+    Option.iter (exp ~allow_system m) eo; dec_fields ~allow_system m dfs 
   | ObjE (bases, efs) ->
-    List.iter (exp m) bases; exp_fields m efs
+    List.iter (exp ~allow_system m) bases; exp_fields ~allow_system m efs
 
   (* Variable access. Dangerous, due to loops. *)
   | (VarE _ | ImportE _ | ImplicitLibE _) -> ()
 
   (* Projections. These are a form of evaluation. *)
   | ProjE (exp1, _)
-  | DotE (exp1, _, _) -> exp m exp1
+  | DotE (exp1, _, _) -> exp ~allow_system m exp1
   | IdxE (exp1, exp2) -> err m e.at
 
   (* Transparent *)
-  | AnnotE (exp1, _) | IgnoreE exp1 | DoOptE exp1 -> exp m exp1
-  | BlockE ds -> List.iter (dec m) ds
+  | AnnotE (exp1, _) | IgnoreE exp1 | DoOptE exp1 -> exp ~allow_system m exp1 
+  | BlockE ds -> List.iter (dec ~allow_system m) ds
 
+  (* 
+     if <system> and we want to allow <system> calls, check.
+     use-case: multi-migration actor bodies where we want to allow e.g., timers. 
+  *)
+  | CallE (_, _, inst, _) 
+    -> (match (allow_system, inst.it) with 
+        | (true, Some(true, _)) -> ()
+        | _ -> err m e.at)
+   
   (* Clearly non-static *)
   | UnE _
   | ShowE _
@@ -78,7 +87,6 @@ let rec exp m e = match e.it with
   | BinE _
   | RelE _
   | AssignE _
-  | CallE _
   | AndE _
   | OrE _
   | WhileE _
@@ -91,16 +99,18 @@ let rec exp m e = match e.it with
   | BangE _
   -> err m e.at
 
-and dec_fields m dfs = List.iter (fun df -> dec m df.it.dec) dfs
+and dec_fields ?(allow_system = false) m dfs = List.iter (fun df -> dec ~allow_system  m df.it.dec) dfs
 
-and exp_fields m efs = List.iter (fun (ef : exp_field) ->
+and exp_fields ?(allow_system = false) m efs = List.iter (fun (ef : exp_field) ->
   if ef.it.mut.it = Var then err m ef.at;
-  exp m ef.it.exp) efs
+  exp ~allow_system m ef.it.exp) efs
 
-and dec m d = match d.it with
+and dec ?(allow_system = false) m d = match d.it with
   | TypD _ | ClassD _ | MixinD _ -> ()
-  | ExpD e -> exp m e
-  | LetD (p, e, fail) -> pat m p; exp m e; Option.iter (exp m) fail
+  | IncludeD _ when allow_system -> ()
+  | ExpD e -> exp ~allow_system m e
+  | LetD (p, e, fail) -> pat m p; exp ~allow_system m e; Option.iter (exp ~allow_system m) fail
+  | VarD (_, e) when allow_system -> exp ~allow_system m e
   | VarD _ | IncludeD _ -> err m d.at
 
 and pat m p = match p.it with
@@ -126,21 +136,11 @@ and pat_field m pf = match pf.it with
   | ValPF(_, p) -> pat m p
   | TypPF(_) -> ()
 
-let prog p =
-  Diag.with_message_store (fun m -> List.iter (dec m) p.it; Some ())
+let prog ?(allow_system = false) p =
+  Diag.with_message_store (fun m -> List.iter (dec ~allow_system m) p.it; Some ())
 
 let rec is_system e = match e.it with
   | CallE (_, _, inst, _) -> (match inst.it with Some (true, _) -> true | _ -> false)
   | IgnoreE e' | AnnotE (e', _) -> is_system e'
   | _ -> false
-
-let actor_dec m d = match d.it with
-  | IncludeD _ -> ()
-  | VarD (_, e) when is_system e -> ()
-  | LetD (_, e, _) when is_system e -> ()
-  | ExpD e when is_system e -> ()
-  | VarD (_, e) -> exp m e
-  | _ -> dec m d
-
-let actor_dec_fields m dfs = List.iter (fun df -> actor_dec m df.it.dec) dfs
 
--- src/mo_frontend/static.mli
+++ src/mo_frontend/static.mli
@@ -1,6 +1,5 @@
 open Mo_def
 
-val exp : Diag.msg_store -> Syntax.exp -> unit
-val dec_fields : Diag.msg_store -> Syntax.dec_field list -> unit
-val prog : Syntax.prog -> unit Diag.result
-val actor_dec_fields : Diag.msg_store -> Syntax.dec_field list -> unit
+val exp : ?allow_system:bool -> Diag.msg_store -> Syntax.exp -> unit
+val dec_fields : ?allow_system:bool -> Diag.msg_store -> Syntax.dec_field list -> unit
+val prog : ?allow_system:bool -> Syntax.prog -> unit Diag.result
