55f151e3962e182ed1b5557fccdc8a5bd952d0e0
Gabor Greif <ggreif@gmail.com>
2026-04-01 09:45:44 +0200

    test: improve accept.mk with THIS_DIR, REPO_ROOT, runtime glob

--- test/accept.mk
+++ test/accept.mk
@@ -1,9 +1,12 @@
 .PHONY: %.accept
+THIS_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
+REPO_ROOT := $(shell git rev-parse --show-toplevel)
+
 %.accept: %.mo
 	../run.sh -a $(RUNFLAGS) $<
-	@ git status -s $< ok
+	@ git -C $(REPO_ROOT) status -s $(CURDIR)/$< $(THIS_DIR)/ok
 	@ echo STAGING: $< $(wildcard ok/$(basename $<).*)
-	@ if [ -n "$(wildcard ok/$(basename $<).*)" ] \
-	; then git add --ignore-errors --update $< $(wildcard ok/$(basename $<).*) \
+	@ if [ -n "$(wildcard $(THIS_DIR)/ok/$(basename $<).*)" ] \
+	; then git add --ignore-errors --update $< $(wildcard $(THIS_DIR)/ok/$(basename $<).*) \
 	; fi
-	@ git status -s $< $(wildcard ok/$(basename $<).*)
+	@ git -C $(REPO_ROOT) status -s $(CURDIR)/$< $(CURDIR)/ok/$(basename $<).*
