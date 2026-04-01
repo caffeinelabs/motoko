971f2304b9d7a42f7c13a0db5920ac333f0d2f65
Gabor Greif <ggreif@gmail.com>
2026-04-01 10:18:55 +0200

    test: silence git rm output with --quiet

--- test/accept.mk
+++ test/accept.mk
@@ -9,5 +9,5 @@ REPO_ROOT := $(shell git rev-parse --show-toplevel)
 	@ set -- $(CURDIR)/ok/$(basename $<).*; \
 	  [ -e "$$1" ] && git add --ignore-errors $(CURDIR)/$< "$$@" || true
 	@ git -C $(REPO_ROOT) status --porcelain -- "$(CURDIR)/ok/$(basename $<).*" | \
-	  awk '/^AD / {print $$2}' | xargs -r git -C $(REPO_ROOT) rm --cached --
+	  awk '/^AD / {print $$2}' | xargs -r git -C $(REPO_ROOT) rm --cached --quiet --
 	@ git -C $(REPO_ROOT) status -s $(CURDIR)/$< $(CURDIR)/ok/$(basename $<).*
