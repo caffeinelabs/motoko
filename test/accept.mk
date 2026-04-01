2e58eb00640e855fa0946244f246b0ef275c81ae
Gabor Greif <ggreif@gmail.com>
2026-04-01 10:13:37 +0200

    test: unstage AD ok-files via git status --porcelain

--- test/accept.mk
+++ test/accept.mk
@@ -8,4 +8,6 @@ REPO_ROOT := $(shell git rev-parse --show-toplevel)
 	@ echo STAGING: $< $(wildcard ok/$(basename $<).*)
 	@ set -- $(CURDIR)/ok/$(basename $<).*; \
 	  [ -e "$$1" ] && git add --ignore-errors $(CURDIR)/$< "$$@" || true
+	@ git -C $(REPO_ROOT) status --porcelain -- "$(CURDIR)/ok/$(basename $<).*" | \
+	  awk '/^AD / {print $$2}' | xargs -r git -C $(REPO_ROOT) rm --cached --
 	@ git -C $(REPO_ROOT) status -s $(CURDIR)/$< $(CURDIR)/ok/$(basename $<).*
