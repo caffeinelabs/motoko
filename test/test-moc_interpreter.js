188ba5489497680d9b212bd0d2ee889de4893d89
Gabor Greif <gabor@dfinity.org>
2026-04-09 02:45:11 +0200

    test: binary search step 4: try 7750 (midpoint of [7600, 7900])

--- test/test-moc_interpreter.js
+++ test/test-moc_interpreter.js
@@ -10,7 +10,7 @@ moc.Motoko.saveFile('empty.mo', '');
 moc.Motoko.saveFile('ok.mo', '1');
 moc.Motoko.saveFile('warn.mo', '2 - 1');
 moc.Motoko.saveFile('bad.mo', '1+');
-moc.Motoko.saveFile('limit.mo', 'var i = 0; while (i < 7900) { i += 1 }; i');
+moc.Motoko.saveFile('limit.mo', 'var i = 0; while (i < 7750) { i += 1 }; i');
 moc.Motoko.saveFile('text.mo', `let s = "${'⛔|'.repeat(10000)}"; s.size()`); // #3822
 
 try {
@@ -43,7 +43,7 @@ try {
       error: null,
     },
     stderr: '',
-    stdout: '7_900 : Nat\n'
+    stdout: '7_750 : Nat\n'
   });
 
   moc.Motoko.setRunStepLimit(5000);
