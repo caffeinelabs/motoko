3d9d6fb9b0cce889e2a05897a6a4076d02d5d93f
Gabor Greif <gabor@dfinity.org>
2026-04-11 14:20:45 +0200

    switch to `nixos-unstable` for now...

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "The Motoko compiler";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
 
