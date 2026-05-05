dfcf9c59651d99d106bbea99a6d6996f4886c3b1
Gabor Greif <gabor.greif@caffeine.ai>
2026-05-06 01:51:13 +0200

    Update accept.mk

--- test/accept.mk
+++ test/accept.mk
@@ -2,8 +2,7 @@
 THIS_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 REPO_ROOT := $(shell git rev-parse --show-toplevel)
 
-%.accept: %.mo
-	../run.sh -a $(RUNFLAGS) $<
+define _stage_results
 	@ git -C $(REPO_ROOT) status -s $(CURDIR)/$< $(CURDIR)/ok
 	@ echo STAGING: $< $(wildcard ok/$(basename $<).*)
 	@ set -- $(CURDIR)/ok/$(basename $<).*; \
@@ -11,3 +10,12 @@ REPO_ROOT := $(shell git rev-parse --show-toplevel)
 	@ git -C $(REPO_ROOT) status --porcelain -- "$(CURDIR)/ok/$(basename $<).*" | \
 	  awk '/^AD / {print $$2}' | xargs -r git -C $(REPO_ROOT) rm --cached --quiet --
 	@ git -C $(REPO_ROOT) status -s $(CURDIR)/$< $(CURDIR)/ok/$(basename $<).*
+endef
+
+%.accept: %.mo
+	run-test -a $(RUNFLAGS) $<
+	$(_stage_results)
+
+%.accept: %.drun
+	run-test -a $(RUNFLAGS) -d $<
+	$(_stage_results)
